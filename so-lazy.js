ac77890914631f26ada9f4d3b76d4dd5476b15fa
吴宝华 <baohua.wu@zerotech.com>
2017-04-05 13:56:54 +0800

    添加注释

--- so-lazy.js
+++ so-lazy.js
@@ -1,8 +1,8 @@
 var lazyload = {
-    imgWrapper: document.getElementsByClassName('img-wrapper'),
-    src: '',
-    setImg: function (index) {
-        if (this.imgWrapper[index].dataset) {
+    imgWrapper: document.getElementsByClassName('img-wrapper'), //图片的包装元素
+    src: '', //图片的链接
+    setImg: function (index) { //添加img标签到包装元素中
+        if (this.imgWrapper[index].dataset) { //考虑到浏览器的兼容性问题，如果浏览器支持元素的自定义属性
             this.src = this.imgWrapper[index].dataset.src;
         } else {
             this.src = this.imgWrapper[index].getAttribute('data-src');
@@ -14,7 +14,7 @@ var lazyload = {
             this.imgWrapper[index].appendChild(img);
         }
     },
-    getHeight: function (obj) {
+    getHeight: function (obj) {  //返回当前元素在当前页面中的高度
         var height = 0;
         if (obj) {
             height += obj.offsetTop;
@@ -27,7 +27,7 @@ window.onscroll = function () {
     var imgWrp = lazyload.imgWrapper;
     for (var i = 0, len = imgWrp.length; i < len; i++) {
         var sub = imgWrp[i];
-        var top = document.documentElement.clientHeight + (document.documentElement.scrollTop || document.body.scrollTop);
+        var top = document.documentElement.clientHeight + (document.documentElement.scrollTop || document.body.scrollTop); //document.documentElement.clientHeight为当前页面视口的高度，document.documentElement.scrollTop || document.body.scrollTop为当前页面超过视口的高度
         var subHeight = lazyload.getHeight(sub);
         if (subHeight < top) {
             setTimeout("lazyload.setImg(" + i + ")", 500);
